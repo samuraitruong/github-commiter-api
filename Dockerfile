537cabaf610929f4ef61a1cdeecf24ff338b9a96
Truong Nguyen <truong.nguyen@tymlez.com>
2023-06-15 17:49:08 +1000

    initial

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:18-apline
+WORKDIR /app
+COPY package.json .
+COPY yarn.lock .
+COPY ./app ./app
+RUN yarn install
+ENV PORT 8080
+EXPOSE 8080
+CMD [ "npm", "start" ]
---
+++ app/server.mjs
@@ -0,0 +1,54 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+const githubAccessToken = process.env.GITHUB_ACCESS_TOKEN;
+const port = process.env.PORT || 9000;
+
+const app = express();
+app.use(express.json());
+
+// Route to handle the commit request
+app.post("/commit", async (req, res) => {
+  try {
+    const { repoOwner, repoName, filePath, commitMessage, content } = req.body;
+
+    // Create the file content
+    const fileContent = Buffer.from(content).toString("base64");
+
+    // Prepare the commit payload
+    const commitPayload = {
+      message: commitMessage,
+      content: fileContent,
+    };
+
+    // Make the API call to GitHub
+    const response = await axios.put(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`,
+      commitPayload,
+      {
+        headers: {
+          Authorization: `Bearer ${githubAccessToken}`,
+          "Content-Type": "application/json",
+          "User-Agent": "Your-App",
+        },
+      }
+    );
+
+    // Handle the response from GitHub
+    if (response.status === 201) {
+      res.json({ message: "File committed successfully." });
+    } else {
+      res.status(response.status).json({ message: "Failed to commit file." });
+    }
+  } catch (error) {
+    console.error("Error committing file:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log("Server listening on port %d", port);
+});
